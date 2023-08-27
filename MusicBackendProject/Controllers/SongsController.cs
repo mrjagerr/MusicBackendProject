e1ec6d7b3bceb83edb24db4bb0631ee7382f57b2
skyeler yearsley <s2krighty@gmail.com>
2023-08-27 15:32:51 -0500

    Update of controller with post method

--- MusicBackendProject/Controllers/SongsController.cs
+++ MusicBackendProject/Controllers/SongsController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MusicBackendProject.Model;
 using static MusicBackendProject.Data.ApplicationDbContect;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -40,14 +42,19 @@ namespace MusicBackendProject.Controllers
 
         // POST api/<SongsController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult Post([FromBody] Song song)
         {
+            _context.Songs.Add(song);
+            _context.SaveChanges();
+            return StatusCode(201, song);
         }
 
         // PUT api/<SongsController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public IActionResult Put(int id,[FromBody] Song song)
         {
+            if (!ModelState.IsValid)
+                return BadRequest("Not a valid model");
         }
 
         // DELETE api/<SongsController>/5
