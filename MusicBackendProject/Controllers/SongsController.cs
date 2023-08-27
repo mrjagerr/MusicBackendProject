6e1ad4e7f62a64a6a58568acbfec38199b0ebe4a
skyeler yearsley <s2krighty@gmail.com>
2023-08-27 14:37:41 -0500

    Update of controller/ Addition of GetAll/Get Method

---
+++ MusicBackendProject/Controllers/SongsController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using static MusicBackendProject.Data.ApplicationDbContect;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MusicBackendProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SongsController : ControllerBase
+    {
+       
+        
+            private readonly ApplicationDbContext _context;
+            public SongsController(ApplicationDbContext context)
+            {
+                _context = context;
+            }
+            // GET: api/<SongsController>
+            // Get All
+            [HttpGet]
+        public IActionResult Get()
+        {
+            var songs =_context.Songs.ToList();
+            return StatusCode(200, songs);
+        }
+
+        // GET api/<SongsController>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var song = _context.Songs.Find(id);
+            if(song == null)
+            {
+                return NotFound();
+            }
+            return Ok(song);
+        }
+        
+
+        // POST api/<SongsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<SongsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<SongsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ MusicBackendProject/Data/ApplicationDbContect.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicBackendProject.Model;
+
+namespace MusicBackendProject.Data
+{
+    public class ApplicationDbContect
+    {
+        public class ApplicationDbContext : DbContext
+        {
+            public DbSet<Song> Songs { get; set; }
+            public ApplicationDbContext(DbContextOptions options) : base(options)
+            {
+
+            }
+        }
+    }
+}
